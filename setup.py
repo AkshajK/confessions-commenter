288ee0fb171b53e660f98f66f5d5b685fd9965ca
Akshaj Kadaveru <akshajk@mit.edu>
2021-05-13 19:49:20 -0400

    v10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="confessionscommenter", # Replace with your own username
-    version="0.0.9",
+    version="0.0.10",
     author="Raul Alcantara, Erick Gbordzoe, Akshaj Kadaveru, Sean Elliott, Helen Lu",
     author_email="shareconfessionscommenter@gmail.com",
     description="Autogenerate Comments for Facebook Posts",
